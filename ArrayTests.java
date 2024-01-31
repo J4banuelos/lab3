8a9d9f56d9fb0c5af83f33ec4f5bf35569449744
J4banuelos <j4banuelos@ucsd.edu>
2024-01-31 15:20:23 -0400

    New Changes

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,17 +2,31 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
-    int[] input1 = { 3 };
-    ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
-	}
+    @Test 
+    public void testReverseInPlaceSingleElement() {
+        int[] input = { 3 };
+        ArrayExamples.reverseInPlace(input);
+        assertArrayEquals(new int[]{ 3 }, input);
+    }
 
+    @Test
+    public void testReversedEmpty() {
+        int[] input = { };
+        assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input));
+    }
 
-  @Test
-  public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
-  }
+    // New test cases
+    @Test
+    public void testReverseInPlaceMultipleElements() {
+        int[] input = {1, 2, 3, 4};
+        ArrayExamples.reverseInPlace(input);
+        assertArrayEquals("Should reverse the array in place", new int[]{4, 3, 2, 1}, input);
+    }
+
+    @Test
+    public void testReversedMultipleElements() {
+        int[] input = {1, 2, 3, 4};
+        int[] expected = {4, 3, 2, 1};
+        assertArrayEquals("Should return a new array with elements in reverse order", expected, ArrayExamples.reversed(input));
+    }
 }
